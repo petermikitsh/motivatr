48472d072ed89c06b2a3a9e9933a2914ad58bd74
Son Nguyen <son@Sons-MacBook-Air.local>
2013-09-13 20:25:39 -0700

    update

--- son/util.js
+++ son/util.js
@@ -6,7 +6,7 @@ function getLocation()
 {
 	if (navigator.geolocation)
 	{
-		navigator.geolocation.getCurrentPosition(showPosition);
+		navigator.geolocation.getCurrentPosition(usePosition);
 	}
 	else
 	{
@@ -14,13 +14,50 @@ function getLocation()
 	}
 }
 
-function showPosition(position)
+function usePosition(position)
 {
-	x.innerHTML = "Latitude: " + position.coords.latitude +
-		"<br>Longitude: " + position.coords.longitude;
+	var lat = position.coords.latitude;
+	var lon = position.coords.longitude;
 
-	console.log("Latitude: " + position.coords.latitude);
-	console.log("Longitude: " + position.coords.longitude);	
+	x.innerHTML = "Latitude: " + lat +
+		"<br>Longitude: " + lon;
+
+	console.log("Latitude: " + lat);
+	console.log("Longitude: " + lon);
+
+	//lat1 = 37.790306;
+	//lon1 = -122.404469;
+
+	var lat1 = 37.783958;
+	var lon1 = -122.392385;
+
+	console.log("distance: ", distance(lat1, lon1, lat, lon));
+	console.log("isCloseEnough: ", isCloseEnough(lat1, lon1, lat, lon))	
+}
+
+Number.prototype.toRad = function()
+{
+	return this * Math.PI / 180;
+}
+
+function distance(lat1, lon1, lat2, lon2)
+{
+	var R = 6371;
+	var x1 = lat2 - lat1;
+	var dLat = x1.toRad();
+	var x2 = lon2 - lon1;
+	var dLon = x2.toRad();
+	var a = Math.sin(dLat/2) * Math.sin(dLat/2) + 
+                Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) * 
+                Math.sin(dLon/2) * Math.sin(dLon/2);  
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+	var d = R * c * 1000;
+	return d;
+}
+
+function isCloseEnough(lat1, lon1, lat2, lon2)
+{
+	return distance(lat1, lon1, lat2, lon2) <= 50;
 }
 
 $("#checkinButton").click(getLocation);
