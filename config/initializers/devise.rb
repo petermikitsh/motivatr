e8a2908cb03ca7a61994efdb899244de22489252
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-09-14 13:14:24 -0700

    devise secret from enviornment variables

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -207,6 +207,8 @@ Devise.setup do |config|
   # The "*/*" below is required to match Internet Explorer requests.
   # config.navigational_formats = ["*/*", :html]
 
+  config.secret_key = ENV['DEVISE_SECRET']
+
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
 
