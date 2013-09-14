c841ae96855ca3aa511bc65776791ac9c65522cc
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-09-13 21:46:30 -0700

    blackmail model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130914043938) do
+ActiveRecord::Schema.define(:version => 20130914044539) do
+
+  create_table "blackmails", :force => true do |t|
+    t.string   "image"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "blackmails", ["user_id"], :name => "index_blackmails_on_user_id"
 
   create_table "challenges", :force => true do |t|
     t.datetime "start"
