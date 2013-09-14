c0a6e0937163eb6604a6754b50f570961d0d4be8
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-09-13 19:11:58 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Motivatr::Application.config.secret_token = 'a90ebdb53d02514d095a063ac31177c105ada6de490243697e4ccdd0aace26e07cc32985bc6ccf187e7e14c79d072fe6c390077e0d847854a204a8aa7648be33'
