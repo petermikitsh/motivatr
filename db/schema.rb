d5c5c980a6038460e9aec3032d59e28e2862b774
Peter Mikitsh <peter.mikitsh@gmail.com>
2013-09-13 21:17:19 -0700

    has_and_belongs_to_many users and groups

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130914035144) do
+ActiveRecord::Schema.define(:version => 20130914041027) do
 
   create_table "groups", :force => true do |t|
     t.integer  "bucket"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
+  end
+
+  create_table "groups_users", :id => false, :force => true do |t|
+    t.integer "group_id"
+    t.integer "user_id"
   end
 
   create_table "users", :force => true do |t|
